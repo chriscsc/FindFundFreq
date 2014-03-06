609c248af02c8299e46302555f4a64541250c270
Chris Cheung <chriscsc@me.com>
2014-03-06 11:20:56 +0800

    remove autocorrelation method

--- findFundFreq.m
+++ findFundFreq.m
@@ -7,15 +7,27 @@ n = length(wave)-1;
 f = 0:fs/n:fs;
 
 wavedft = fft(wave);
-subplot(211);plot(f, (wavedft));
+subplot(211);plot(f, abs(wavedft));
 ms20 = fs/50;
 ms2 = fs/500;
 d = (-ms20:ms20)/fs;
 r = xcorr(wave, ms20, 'coeff');
 subplot(212);plot(d, r);
 
-r = r(ms20+1:2*ms20+1);
-[rmax, tx] = max(r(ms2:ms20))
-rmax*fs/n
+fn = round(n/fs)
+
+u= 15;
+
+idx = find(real(wavedft)>u)/fn
+
+fs 
+n
+% just look at region corresponding to positive delays
+r=r(ms20+1:2*ms20+1);
+[rmax,tx]=max(r(ms2:ms20))
+fprintf('rmax=%g Fx=%gHz\n',rmax,fs/(ms2+tx-1));
+
+output = idx(1);
+
 end
 
